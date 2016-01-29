00640e642a1c432ebf5995f6a5b44e4afe53dad4
Андрей Большов <bolshov@uniweb.ru>
2016-01-29 20:03:11 +0300

    add port option

--- lib/warden/clients/publisher.rb
+++ lib/warden/clients/publisher.rb
@@ -1,10 +1,10 @@
 module Warden
   module Clients
     class Publisher
-      def initialize ip
+      def initialize ip, port = 8080
         EM.epoll
         EM.run do
-          conn = EventMachine::WebSocketClient.connect("ws://#{ip}:8080/")
+          conn = EventMachine::WebSocketClient.connect("ws://#{ip}:#{port}/")
           conn.callback do
             puts "Open socket"
             conn.send_msg JSON.dump ["warden"]
