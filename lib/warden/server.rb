a1826356d5304be45264eb3fcff629c6d959553f
Андрей Большов <bolshov@uniweb.ru>
2016-01-29 19:01:38 +0300

    add options

--- lib/warden/server.rb
+++ lib/warden/server.rb
@@ -1,11 +1,14 @@
 module Warden
   class Server
-    def self.start
+    # Запускам сервис. В качестве аргумента передаем хеш с ключами
+    #    address: ip на котором стартуем сервер
+    #    port: port на котором стартуем сервер
+    def self.start options = {}
       EM.epoll
       EM.run {
         hallway =  Channels::Hallway.new
 
-        EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
+        EM::WebSocket.run(:host => options[:address], :port => options[:port]) do |ws|
           ws.onopen { |handshake|
             puts "WebSocket connection open"
             @channel = hallway
